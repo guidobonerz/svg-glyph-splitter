c0177b0e6d96a8974c02c82e4f4853320d7a9d28
guidobonerz <guido.bonerz@drazil.de>
2022-12-19 23:10:48 +0100

    update

--- glyphsplitter.py
+++ glyphsplitter.py
@@ -22,14 +22,14 @@ with open(sys.argv[1], 'r') as r:
         filename = filename.group(1) if filename else str(i + 1).rjust(3, '0')
         if isDuotone:
             filename = filename[:-8]
-        print("glyph %d is %s" % (i, glyphs[i]))
+        print("glyph %d is %s" % (i, filename))
         with open(filename + ".svg", 'w') as w:
             w.write('<?xml version="1.0" standalone="no"?>\n')
             w.write(
-                '<svg width="1500" height="1500" viewBox="0 0 1500 1500" version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
+                '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
             w.write(glyphs[i].replace(
-                '<glyph', '<path style="fill: #8E44AD" transform="scale(1, -1) translate(0, -1500)"') + '\n')
+                '<glyph', '<path style="fill: #8E44AD" transform="scale(1, -1) translate(0, -512)"') + '\n')
             if (isDuotone):
                 w.write(glyphs[i+size].replace(
-                    '<glyph', '<path style="fill: #3498DB" transform="scale(1, -1) translate(0, -1500)"') + '\n')
+                    '<glyph', '<path style="fill: #3498DB" transform="scale(1, -1) translate(0, -512)"') + '\n')
             w.write('</svg>')
